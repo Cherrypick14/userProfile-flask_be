a8b7c3ab78d90e5e83cb6d0bbf9dbf6e677c9800
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-15 16:52:05 +0300

    add update and delete tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,6 @@
+import json
+from unicodedata import name
+from urllib import response
 from api.models import User
 
 
@@ -23,4 +26,22 @@ def test_fetch_users(client, init_database):
 
     path = "api/v1/users"
     response = client.get(path)
+    assert response.status_code == 200
+
+
+def test_update_user(client, init_database):
+    path ="/api/v1/3/update"
+    response =client.post(path,
+     json ={
+         "id":5,
+         "name":"hunter",
+         "email":"hunterfields@gmail.com",
+     },
+    )
+    assert response.status_code == 200
+
+def test_delete_user(client, init_database):
+    path= "/api/v1/5/delete"
+    response= client.post(path)
+
     assert response.status_code == 200
