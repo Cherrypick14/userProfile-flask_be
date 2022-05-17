5d27b77d414f510f826dff65f86155e05c965a38
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-17 12:18:05 +0300

    add list of test users

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -17,31 +17,31 @@ def test_new_user(client, init_database):
 
 def test_fetch_user(client, init_database):
 
-    path = "/api/v1/2"
+    path = "/api/v1/1"
     response = client.get(path)
-    assert response.status_code == 200
+    assert b'{"email":"test1@gmail.com","id":1,"name":"Test User 1"}\n' in response.data
 
 
 def test_fetch_users(client, init_database):
 
     path = "api/v1/users"
     response = client.get(path)
-    assert response.status_code == 200
+    assert b'[{"email":"test1@gmail.com","id":1,"name":"Test User 1"},{"email":"test2@gmail.com","id":2,"name":"Test User 2"},{"email":"test3@gmail.com","id":3,"name":"Test User 3"}]\n' in response.data
 
 
 def test_update_user(client, init_database):
-    path ="/api/v1/3/update"
+    path ="/api/v1/1/update"
     response =client.post(path,
      json ={
-         "id":5,
+         "id":1,
          "name":"hunter",
          "email":"hunterfields@gmail.com",
      },
     )
-    assert response.status_code == 200
+    assert b'{"email":"hunterfields@gmail.com","id":1,"name":"hunter"}\n' in response.data
 
 def test_delete_user(client, init_database):
     path= "/api/v1/5/delete"
     response= client.post(path)
-
     assert response.status_code == 200
+    assert b'User has been deleted' in response.data
