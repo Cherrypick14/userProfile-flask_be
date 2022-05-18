2b48b64ddf88c90b62f691019beb1f0a3ca973df
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-18 03:35:07 +0300

    add bcrypt

--- api/routes.py
+++ api/routes.py
@@ -1,10 +1,11 @@
-from api.app import create_app, db
-from api.models import User,user_schema, users_schema
-from flask import request,redirect,jsonify
+from api.app import create_app, db, bcrypt
+from api.models import User, user_schema, users_schema
+from flask import request, jsonify
 
 app = create_app()
 
-@app.route("/api/v1/add", methods=['GET','POST'])
+
+@app.route("/api/v1/add", methods=['GET', 'POST'])
 def create():
 
     name = request.json["name"]
@@ -12,32 +13,36 @@ def create():
     password = request.json["password"]
 
     try:
-       user = User(name=name,email=email,password=password)
-       db.session.add(user)
-       db.session.commit()
-       
-       created_user = User.query.filter_by(email=email).first()
+        hashed_password = bcrypt.generate_password_hash(password)
+        user = User(name=name, email=email, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+
+        new_user = User.query.filter_by(email=email).first()
 
     except Exception as e:
-        print(f"Error {e}")
-    
-    return user_schema.dump(created_user)
 
-@app.route("/api/v1/<int:id>",methods=['GET','POST'])
+        print(f"user exists{e}")
+
+    return user_schema.dump(new_user)
+
+
+@app.route("/api/v1/<int:id>", methods=['GET', 'POST'])
 def RetrieveSingleUser(id):
     user = User.query.filter_by(id=id).first()
-    return user_schema.dump(user) 
+    return user_schema.dump(user)
 
-@app.route("/api/v1/users", methods=['GET','POST'])
+
+@app.route("/api/v1/users", methods=['GET', 'POST'])
 def RetrieveSingleUsers():
     users = User.query.all()
-    all_users = users_schema.dump(users) 
+    all_users = users_schema.dump(users)
     return jsonify(all_users)
 
 
-@app.route("/api/v1/<int:id>/update", methods=['GET','POST'])
+@app.route("/api/v1/<int:id>/update", methods=['GET', 'POST'])
 def update(id):
-    user=User.query.filter_by(id=id).first()
+    user = User.query.filter_by(id=id).first()
 
     if user:
         name = request.json["name"]
@@ -48,15 +53,16 @@ def update(id):
 
         db.session.commit()
 
-    return user_schema.dump(user) 
+    return user_schema.dump(user)
 
-@app.route("/api/v1/<int:id>/delete", methods=['GET','POST'])
+
+@app.route("/api/v1/<int:id>/delete", methods=['GET', 'POST'])
 def delete(id):
-    user=User.query.filter_by(id=id).first()
+    user = User.query.filter_by(id=id).first()
     if request.method == "POST":
         if user:
-           db.session.delete(user)
-           db.session.commit()
+            db.session.delete(user)
+            db.session.commit()
 
     return jsonify("User has been deleted")
 
