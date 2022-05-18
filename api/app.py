6062d8ae27bd0a0b0f42c99092964f2d6d9b869c
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-18 03:36:47 +0300

    add bcrypt obj

--- api/app.py
+++ api/app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 import config
 
 db = SQLAlchemy()
 migrate = Migrate()
 ma = Marshmallow()
+bcrypt= Bcrypt()
 
 
 def create_app():
@@ -14,10 +16,12 @@ def create_app():
 
     app.config.from_object(config.DevelopmentConfig)
 
-  
+    
     db.init_app(app)
+    bcrypt.init_app(app)
     migrate.init_app(app, db)
     ma.init_app(db)
+   
 
 
     return app
